91d6879d69376c3f1ef137f66126c000ceac7dcd
Alex <steina@student.ethz.ch>
2020-07-05 15:09:28 +0200

    adapt compiler

--- prog/compile_execute.sh
+++ prog/compile_execute.sh
@@ -6,7 +6,7 @@ mkdir -p PopsGrids
 mkdir -p PassengersGrids
 mkdir -p MigrationRateGrids
 
-g++ -o demon demon.cpp -I/usr/local/include/boost/ -lm #compile
+g++ -o demon demon.cpp -I/usr/local/boost_1_73_0/ -lm #compile
 
 ./demon  ./ init_conf_file.dat #execute
 
